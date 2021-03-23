868607ce0b4389087d0d5d73eecce6fb144f1ec2
Salman <m.salmank9991@gmail.com>
2021-03-23 07:57:57 -0700

    done

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,28 @@
+// @ts-ignore
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { setupServer } from './services/mirage/server';
+
+import './index.css';
+
+
+
+import { Provider } from 'react-redux';
+import store from './store';
+// ...
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+if (process.env.NODE_ENV === 'development') {
+  setupServer();
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
